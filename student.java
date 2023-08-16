22cc41a95599a5afd1e704836439c890835cd64c
vijay-3209 <142398889+vijay-3209@users.noreply.github.com>
2023-08-16 15:50:18 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class student
+{
+public static void main(String[]rsml)
+{
+ int x,y;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter value for x");
+x=sc.nextInt();
+System.out.println("enter the value for y");
+y=sc.nextInt();
+if(x>y)
+{
+System.out.println("x is greater than y");
+}
+else
+{
+System.out.println("y is greater than x");
+}
+}
+}
